16f2fdde19cd5f4df5464147f197b31f2c7bf019
unknown <folastylez@gmail.com>
2023-06-12 12:30:51 +0100

    animated carousel

---
+++ src/persons.ts
@@ -0,0 +1,54 @@
+export const persons = [
+  {
+    img: "",
+    title: "",
+    name: "",
+  },
+  {
+    img: "https://wallpapers.com/images/high/los-angeles-lakers-lebron-james-lted0b86v71lxhhu.webp",
+    title: "Los Angeles Lakers Power Forward Lebron James",
+    name: "Lebron James",
+  },
+  {
+    img: "https://wallpapers.com/images/high/shai-gilgeous-alexander-happy-face-milwaukee-bucks-7n0iroaqzmev1wur.webp",
+    title:
+      "Oklahoma City Thunder Point Guard and Shooting Guard Shai Gilgeous-Alexander",
+    name: "Shai Gilgeous-Alexander",
+  },
+  {
+    img: "https://wallpapers.com/images/high/giannis-antetokounmpo-nba-champion-najlouzbtboz7j9e.webp",
+    title:
+      "Milwaukee Bucks Power Forward Center and Forward Giannis Antetokounmpo",
+    name: "Giannis Antetokounmpo",
+  },
+  {
+    img: "https://wallpapers.com/images/high/steph-curry-with-subtle-smile-vzmg538ysma264gn.webp",
+    title: "Golden State Warriors Point Guard Stephen Curry",
+    name: "Stephen Curry",
+  },
+  {
+    img: "https://wallpapers.com/images/high/luka-doncic-rookie-of-the-year-fnbq7lha3wgb920m.webp",
+    title: "Dallas Mavericks Point Guard Shooting Guard Luka Dončić",
+    name: "Luka Dončić",
+  },
+  {
+    img: "https://wallpapers.com/images/high/cool-french-national-football-team-kylian-mbappe-mr7el8zv93gaqda8.webp",
+    title: "French National Football Star Kylian Mbappe",
+    name: "Kylian Mbappe",
+  },
+  {
+    img: "https://wallpapers.com/images/high/marcus-rashford-happy-ke651w8wfric7pz4.webp",
+    title: "Manchester United and England Forward Marcus Rashford",
+    name: "Marcus Rashford",
+  },
+  {
+    img: "https://wallpapers.com/images/high/serena-williams-wilson-racket-sjd3mv2x5pobpug6.webp",
+    title: "legendary tennis player Serena Williams",
+    name: "Serena Williams",
+  },
+  {
+    img: "",
+    title: "",
+    name: "",
+  },
+];
